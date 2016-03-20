e5ea02360e7a734a4fe8b27afb7b6ce69d6bacd1
Jérémy Demeule <dj_mulder@macbook-pro.djduron.no-ip.org>
2016-03-20 16:58:03 +0100

    Initial Commit

---
+++ mrl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mrl
+//
+//  Created by Jérémy Demeule on 20/03/16.
+//  Copyright © 2016 Jérémy Demeule. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
